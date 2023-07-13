f8983d95d22a663f41a7da18766c67a533da0bcc
Arvil Meña <arvilmena@gmail.com>
2023-07-13 12:51:16 +0800

    eslint + npm run lint

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,5 @@
 import { Button } from '@LottoLotto/Ui';
 export default async function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.scss file.
-   */
   return (
     <div className="container mx-auto">
       <div className="prose">
